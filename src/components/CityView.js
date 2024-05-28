fb96ccb77015bb75301afc0aaf2b5505fc9960a7
Steven Burrell <sburrell23@gmail.com>
2024-05-27 22:46:36 -0600

    fixing flash still

--- src/components/CityView.js
+++ src/components/CityView.js
@@ -3,41 +3,41 @@ import axios from 'axios';
 
 function CityView() {
     const [neighborhoods, setNeighborhoods] = useState([]);
+    const [previousNeighborhoods, setPreviousNeighborhoods] = useState([]);
 
     useEffect(() => {
         getNeighborhoods();
         const interval = setInterval(() => {
             getNeighborhoods();
-        }, 5000); // 5 seconds
+        }, 1000); // 5 seconds
         return () => clearInterval(interval);
-    }, []);
+    }, [getNeighborhoods]);
 
     function getNeighborhoods() {
         axios.get('http://localhost:8080/neighborhoodStats')
             .then(response => {
                 setNeighborhoods(response.data);
+                setPreviousNeighborhoods(neighborhoods);
             })
             .catch(error => {
                 console.log(error);
             });
     }
 
-    
-
     return (
         <div className="container" style={{width:"100%"}}>
             <h2>City View</h2>
             <div className='row'>
-                {neighborhoods.map((neighborhood, index) => (
-                    <div key={index} className="col-3 mb-2">
-                        <div className={`card nhCard ${neighborhood.changed ? 'flash' : ''}`}>
-                            <div className="card-body">
-                                <h5 className="card-title">{neighborhood.neighborhood}</h5>
-                                <p className="card-text">Population: {neighborhood.count}</p>
-                            </div>
+            {neighborhoods.map((neighborhood, index) => (
+                <div key={index} className="col-3 mb-2">
+                    <div className={`card nhCard ${neighborhood.count > previousNeighborhoods[index]?.count ? 'flash' : ''}`}>
+                        <div className="card-body">
+                            <h5 className="card-title">{neighborhood.neighborhood}</h5>
+                            <p className="card-text">Population: {neighborhood.count}</p>
                         </div>
                     </div>
-                ))}
+                </div>
+            ))}
             </div>
         </div>
     );
